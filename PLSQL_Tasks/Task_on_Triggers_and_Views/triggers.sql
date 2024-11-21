dd7ef0265ba5aa4fba24350f7866884e9076b6b9
harish7533 <harishbalamurugan.2002@gmail.com>
2024-11-21 16:10:54 +0530

    plsql tasks->updated triggers logic

--- PLSQL_Tasks/Task_on_Triggers_and_Views/triggers.sql
+++ PLSQL_Tasks/Task_on_Triggers_and_Views/triggers.sql
@@ -1,4 +1,25 @@
--- Starting triggers when there is DML Command in the Employee Table 
+-- Starting triggers when there is DML Command in the Employee Table
+delimiter &&
+create trigger insert_trigger
+after insert on Employee
+for each row
+begin
+    DECLARE employee_count INT;
+    SELECT COUNT(emp_Id) INTO employee_count FROM Employee;
+    INSERT INTO employee_log (log_time, message, emp_Id, emp_Name, employee_count)
+    VALUES (CURRENT_DATE(), 'Inserted', NEW.emp_Id, NEW.emp_Name, employee_count);
+end &&
+delimiter ;
+
+INSERT INTO Employee
+(emp_Id, emp_Name, emp_Email, emp_DOB, emp_DOJ, emp_Salary, emp_Age, emp_BloodGroup, emp_Address, emp_Designation, emp_Location, emp_Mobile_number) 
+VALUES
+(1020,'Ajmal', 'ajmalameer@gmail.com', '1999-03-23', '2021-09-11', 38764.90, 22, 'B+', '13/210,Holy Goat Street, Canria', 'Training', 'Kochi', 8374654762);
+
+select * from employee_log;
+
+drop trigger learning_plsql.insert_trigger;
+ 
 delimiter //
 create trigger trg_after_update_salary
 on Employee
